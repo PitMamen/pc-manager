0d9ea8816df55cd5a140caf134d7bd536442c91f
fushengbo <shengbo.fu@bitvalue.com.cn>
2022-06-18 14:14:41 +0800

    订单管理收款方信息增加

--- src/views/system/order/index.vue
+++ src/views/system/order/index.vue
@@ -6,11 +6,26 @@
           <a-row :gutter="48">
             <a-col :md="5" :sm="24">
               <a-form-item label="订单编号">
-                <a-input v-model="queryParams.orderId" allow-clear placeholder="请输入" @keyup.enter="$refs.table.refresh(true)"/>
+                <a-input
+                  v-model="queryParams.orderId"
+                  allow-clear
+                  placeholder="请输入"
+                  @keyup.enter="$refs.table.refresh(true)"
+                />
               </a-form-item>
             </a-col>
 
             <a-col :md="6" :sm="24">
+              <a-form-item label="收款方">
+                <a-select allow-clear v-model="queryParams.functionType" placeholder="请选择状态">
+                  <a-select-option v-for="(item, index) in functionData" :key="index" :value="item.id">{{
+                    item.value
+                  }}</a-select-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+
+            <!-- <a-col :md="6" :sm="24">
               <a-form-item label="下单时间">
                 <a-date-picker format="YYYY-MM-DD" v-model="queryParams.startOrderTime" />
               </a-form-item>
@@ -19,6 +34,12 @@
             <a-col :md="4" :sm="24">
               <a-form-item label="">
                 <a-date-picker format="YYYY-MM-DD" v-model="queryParams.endOrderTime" />
+              </a-form-item>
+            </a-col> -->
+
+            <a-col :md="5" :sm="24">
+              <a-form-item label="下单时间">
+                <a-range-picker @change="onChange" />
               </a-form-item>
             </a-col>
 
@@ -34,7 +55,12 @@
 
             <a-col :md="4" :sm="24" v-if="false">
               <a-form-item label="就诊人">
-                <a-input v-model="queryParams.keyWord" allow-clear placeholder="请输入就诊人" @keyup.enter="$refs.table.refresh(true)" />
+                <a-input
+                  v-model="queryParams.keyWord"
+                  allow-clear
+                  placeholder="请输入就诊人"
+                  @keyup.enter="$refs.table.refresh(true)"
+                />
               </a-form-item>
             </a-col>
 
@@ -51,9 +77,6 @@
         </a-form>
       </div>
 
-      <!-- 去掉勾选框 -->
-      <!-- :rowSelection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }" -->
-      <!-- :row-selection="rowSelection" -->
       <s-table
         ref="table"
         size="default"
@@ -102,10 +125,16 @@ export default {
         { code: 4, value: '待收货' },
         { code: 5, value: '订单取消' },
       ],
+      functionData: [
+        { code: -1, id: '', value: '全部' },
+        { code: 0, id: '0', value: '健康在线' },
+        { code: 1, id: '1', value: '互联网医院' },
+      ],
       partChoose: '',
 
       queryParams: {
-        functionType: 0, //业务类型：0代表套餐，1代表云门诊
+        // functionType 0:健康在线 1:互联网医院
+        functionType: -1, //业务类型：健康在线 1:互联网医院
         endOrderTime: '',
         startOrderTime: '',
         orderId: '',
@@ -123,6 +152,10 @@ export default {
         {
           title: '下单时间',
           dataIndex: 'orderTime',
+        },
+        {
+          title: '收款方',
+          dataIndex: 'functionTypeName',
         },
         {
           title: '就诊人',
@@ -152,6 +185,9 @@ export default {
       loadData: (parameter) => {
         if (this.queryParams.status == -1) {
           delete this.queryParams.status
+        }
+        if (this.queryParams.functionType == -1) {
+          this.queryParams.functionType = ''
         }
 
         if (this.queryParams.startOrderTime && this.queryParams.endOrderTime) {
@@ -183,6 +219,11 @@ export default {
 
             this.$set(res.data.rows[i], 'userNameIn', res.data.rows[i].userInfo.userName)
             this.$set(res.data.rows[i], 'userIdIn', res.data.rows[i].userInfo.userId)
+            this.$set(
+              res.data.rows[i],
+              'functionTypeName',
+              res.data.rows[i].functionType == 0 ? '健康在线' : '互联网医院'
+            )
 
             //订单状态（1：待支付 2：已完成 3：部分支付 4：待收货 5：订单取消）
             if (res.data.rows[i].status == 1) {
@@ -203,8 +244,7 @@ export default {
     }
   },
 
-  created() {
-  },
+  created() {},
 
   methods: {
     formatDate(date) {
@@ -253,6 +293,11 @@ export default {
         .catch((err) => {
           this.$message.error('导出错误：' + err.message)
         })
+    },
+
+    onChange(momentArr, dateArr) {
+      this.queryParams.startOrderTime = dateArr[0]
+      this.queryParams.endOrderTime = dateArr[1]
     },
 
     downloadfile(res) {
