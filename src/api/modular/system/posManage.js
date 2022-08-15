1222054e3dc10cb2a84cd3464d12d0776ab33f49
fushengbo <shengbo.fu@bitvalue.com.cn>
2022-08-15 17:45:47 +0800

    问卷列表调试

--- src/api/modular/system/posManage.js
+++ src/api/modular/system/posManage.js
@@ -85,21 +85,38 @@ export function getAllArticlesTeach(parameter) {
   })
 }
 
+// /**
+//  * 获取问卷
+//  *
+//  */
+// export function getAllQuestions(parameter) {
+//   parameter.start = parameter.pageNo
+//   var newPara = JSON.parse(JSON.stringify(parameter))
+//   delete newPara.pageSize
+//   delete newPara.start
+//   delete newPara.pageNo
+//   return axios({
+//     url: '/patient/qryQuestByKeyWord?start=' + parameter.start + '&pageSize=' + parameter.pageSize,
+//     method: 'get',
+
+//     data: newPara
+//   })
+// }
+
 /**
  * 获取问卷
  *
  */
 export function getAllQuestions(parameter) {
-  parameter.start = parameter.pageNo
-  var newPara = JSON.parse(JSON.stringify(parameter))
-  delete newPara.pageSize
-  delete newPara.start
-  delete newPara.pageNo
   return axios({
-    url: '/patient/qryQuestByKeyWord?start=' + parameter.start + '&pageSize=' + parameter.pageSize,
+    url: '/bdcApi/health/doctor/qryQuestByKeyWord',
     method: 'get',
+    params: {
+      start:parameter.pageNo,
+      pageSize:parameter.pageSize,
+      typeName:parameter.typeName
+    },
 
-    data: newPara
   })
 }
 
@@ -1209,13 +1226,13 @@ export function refundByAdmin(data) {
  * 
  * 获取基本信息和更多信息
  */
- export function getBaseInfo(data) {
+export function getBaseInfo(data) {
   return axios({
     url: '/accountapi/userInfo/getBaseInfo',
     method: 'get',
     params: data,
   })
-  }
+}
 
 /**
  * 
@@ -1233,7 +1250,7 @@ export function qryRevisitPatientList(data) {
  * 
  * 查询随访病人列表
  */
- export function statRevisit(data) {
+export function statRevisit(data) {
   return axios({
     url: '/bdcApi/revisit/statRevisit',
     method: 'post',
