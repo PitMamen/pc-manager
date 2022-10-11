a5a98af6ee66f734e71bb6317b7c441053e1b59d
fushengbo <shengbo.fu@bitvalue.com.cn>
2022-10-11 10:36:20 +0800

    页面基础搭建

---
+++ src/views/system/servicewise/serviceList.vue
@@ -0,0 +1,83 @@
+<template>
+        <a-card :bordered="false">
+          <div class="table-page-search-wrapper" v-if="hasPerm('sysPos:page')">
+            <a-form layout="inline">
+              <a-row :gutter="48">
+                <a-col :md="3" :sm="24">
+                  <a-form-item label="">
+                    <a-button type="primary" @click="addPlan">新增card</a-button>
+                  </a-form-item>
+                </a-col>
+
+
+              </a-row>
+            </a-form>
+          </div>
+
+
+          <!-- <add-form ref="addForm" @ok="handleOk" /> -->
+          <!-- <edit-form ref="editForm" @ok="handleOk" /> -->
+        </a-card>
+</template>
+
+
+<script>
+  import { sysPosAdd } from '@/api/modular/system/posManage'
+  export default {
+    data () {
+      return {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 5 }
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 15 }
+        },
+        visible: false,
+        confirmLoading: false,
+        form: this.$form.createForm(this)
+      }
+
+
+    },
+    methods: {
+      //初始化方法
+      add (record) {
+        this.visible = true
+      },
+
+      addPlan(){
+        this.$message.info('clicked')
+      },
+
+      handleSubmit () {
+        const { form: { validateFields } } = this
+        this.confirmLoading = true
+        validateFields((errors, values) => {
+          if (!errors) {
+            sysPosAdd(values).then((res) => {
+              if(res.success){
+                this.$message.success('新增成功')
+                this.visible = false
+                this.confirmLoading = false
+                this.$emit('ok', values)
+                this.form.resetFields();
+              }else{
+                this.$message.error('新增失败：'+res.message)
+              }
+            }).finally((res) =>{
+              this.confirmLoading = false
+            })
+          } else {
+            this.confirmLoading = false
+          }
+        })
+      },
+      handleCancel () {
+        this.form.resetFields();
+        this.visible = false
+      }
+    }
+  }
+</script>
---
+++ src/views/system/servicewise/serviceWise.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <!-- <a-tabs default-active-key="1" @change="callback" style="width: 60%"> -->
+    <a-tabs default-active-key="1" @change="callback">
+      <a-tab-pane key="1" tab="随访方案">
+        <service-list ref="serviceList" @ok="handleOk" />
+      </a-tab-pane>
+
+      <a-tab-pane key="2" tab="微信模板" force-render>
+      </a-tab-pane>
+
+      <a-tab-pane key="3" tab="短信模板">
+
+      </a-tab-pane>
+      <a-tab-pane key="4" tab="随访名单"></a-tab-pane>
+    </a-tabs>
+  </div>
+</template>
+
+<script>
+// import { STable } from '@/components'
+import {
+
+  deleteAppVersion,
+  qryRevisitPatientList,
+} from '@/api/modular/system/posManage'
+
+import serviceList from './serviceList'
+
+
+
+export default {
+  components: {
+    // STable,
+    // addForm,
+    // editForm,
+    serviceList,
+    // addFormDispatch,
+    // editFormDispatch,
+
+    // statDetail,
+    // statSolve,
+  },
+
+  data() {
+    return {
+     
+    }
+  },
+
+  created() {
+
+  },
+
+  methods: {
+
+  },
+}
+</script>
+
+<style lang="less">
+.div-service {
+  width: 100%;
+  overflow: hidden;
+  height: 100%;
+
+  .card-right {
+    overflow: hidden;
+    width: 100% !important;
+
+    .table-operator {
+      margin-bottom: 18px;
+    }
+    button {
+      margin-right: 8px;
+    }
+
+    .title {
+      background: #fff;
+      font-size: 18px;
+      font-weight: bold;
+      color: #000;
+    }
+  }
+}
+
+</style>
