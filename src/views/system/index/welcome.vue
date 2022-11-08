cefbbfad1d6417f4539372bf89227d331623bbed
tangjie <tangjie@gitee.com>
2022-11-08 11:10:25 +0800

    Do statics some

--- src/views/system/index/welcome.vue
+++ src/views/system/index/welcome.vue
@@ -1,13 +1,19 @@
 <template>
-  <a-card :bordered="false" style="display: flex; justify-content: center; height: 100%">
-    <div style="margin: 100px auto">
-      <img src="~@/assets/welcome.png" class="logo" alt="logo" />
+  <a-card
+    :bordered="false"
+    style="display: flex;justify-content: center;height: calc(100% - 39px);background: #E8F3FF;"
+  >
+    <div style="margin: 200px auto;">
+      <img
+        src="~@/assets/login/chahua.png"
+        alt="logo"
+        style="width: 425px;height: auto;"
+      />
     </div>
   </a-card>
 </template>
 
 <script>
-
 </script>
 
 <style lang="less" scoped>
