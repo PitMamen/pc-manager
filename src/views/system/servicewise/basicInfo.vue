34ef714d10d69aeaf9092a49da18ec2679bd60f7
fushengbo <shengbo.fu@bitvalue.com.cn>
2022-12-07 18:17:30 +0800

    提交基本信息接口调试

--- src/views/system/servicewise/basicInfo.vue
+++ src/views/system/servicewise/basicInfo.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- <div style="height: 500px; width: 100%"> -->
   <div class="inner-wrap">
-    <div v-if="jbxx" style="overflow-y: auto; height: 380px;padding-right: 10px;padding-bottom: 10px;">
+    <div v-if="jbxx" style="overflow-y: auto; height: 380px; padding-right: 10px; padding-bottom: 10px">
       <!-- <span>{{ jbxx.name }}</span> -->
       <div class="div-title">
         <div class="div-line-blue"></div>
@@ -10,40 +10,40 @@
       <div class="div-basic">
         <div class="div-line-wrap">
           <div class="div-item-four">
-            姓名：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            姓名：<span style="text-decoration: underline">{{ jbxx.xm }}</span>
           </div>
           <div class="div-item-four">
-            性别：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            性别：<span style="text-decoration: underline">{{ jbxx.xbs }}</span>
           </div>
           <div class="div-item-four">
-            出生日期：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            出生日期：<span style="text-decoration: underline">{{ patientInfo.baseInfo.birthday }}</span>
           </div>
           <div class="div-item-four">
-            年龄：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            年龄：<span style="text-decoration: underline">{{ jbxx.nl }}</span>
           </div>
         </div>
 
         <div class="div-line-wrap">
           <div class="div-item-four">
-            民族：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            国籍：<span style="text-decoration: underline">{{ jbxx.cismain.gjmc }}</span>
           </div>
           <div class="div-item-four">
-            民族：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            民族：<span style="text-decoration: underline">{{ jbxx.yymzmc }}</span>
           </div>
           <div class="div-item-four">
-            新生儿出生体重：<span style="text-decoration: underline">{{ jbxx.name }}</span
+            新生儿出生体重：<span style="text-decoration: underline">{{ jbxx.fgg || '-' }}</span
             >&nbsp;克
           </div>
           <div class="div-item-four">
-            新生儿入院体重：<span style="text-decoration: underline">{{ jbxx.name }}</span
+            新生儿入院体重：<span style="text-decoration: underline">{{ jbxx.cismain.xserytz || '-' }}</span
             >&nbsp;克
           </div>
         </div>
 
         <div class="div-line-wrap">
           <div class="div-item-two">
-            出生地址：<span style="text-decoration: underline; width: 200px; display: inline-block">{{
-              jbxx.name
+            出生地址：<span style="text-decoration: underline; width: 86%; display: inline-block">{{
+              jbxx.cismain.csd
             }}</span>
             <!-- 出生地址：<span style="border-bottom: 1px solid #333; width: 200px; display: inline-block">{{ -->
             <!-- jbxx.name
@@ -54,97 +54,96 @@
             <span style="text-decoration: underline"></span>
           </div>
           <div class="div-item-four">
-            籍贯：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            籍贯：<span style="text-decoration: underline">{{ jbxx.cismain.jg }}</span>
           </div>
         </div>
 
         <div class="div-line-wrap">
           <div class="div-item-four">
-            身份证号：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            身份证号：<span style="text-decoration: underline">{{ patientInfo.baseInfo.identificationNo }}</span>
           </div>
           <div class="div-item-four">
-            职业：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            职业：<span style="text-decoration: underline">{{ jbxx.cismain.zybm }}</span>
           </div>
           <div class="div-item-four">
-            婚姻：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            婚姻：<span style="text-decoration: underline">{{ jbxx.cismain.hyzkmc }}</span>
+          </div>
+        </div>
+
+        <!-- TODO -->
+        <!-- csd  overflow: hidden; text-overflow: ellipsis; white-space: nowrap  限制一行 -->
+        <div class="div-line-wrap">
+          <div class="div-item-two">
+            户籍地址：<span style="text-decoration: underline; width: 86%; display: inline-block">{{
+              jbxx.cismain.hkdz
+            }}</span>
+          </div>
+
+          <div class="div-item-four">
+            邮编：<span style="text-decoration: underline">{{ jbxx.cismain.hkyb }}</span>
           </div>
         </div>
 
         <div class="div-line-wrap">
           <div class="div-item-two">
-            户籍地址：<span style="text-decoration: underline; width: 200px; display: inline-block"
-              >飞飞飞哥哥哥日哥哥和认同和{{ jbxx.name }}</span
-            >
-            <!-- 出生地址：<span style="border-bottom: 1px solid #333; width: 200px; display: inline-block">{{ -->
-            <!-- jbxx.name
-            }}</span> -->
-          </div>
-
-          <div class="div-item-four">
-            邮编：<span style="text-decoration: underline">{{ jbxx.name }}</span>
-          </div>
-        </div>
-
-        <div class="div-line-wrap">
-          <div class="div-item-two">
-            现居地址：<span style="text-decoration: underline; width: 200px; display: inline-block">{{
-              jbxx.name
+            现居地址：<span style="text-decoration: underline; width: 86%; display: inline-block">{{
+              jbxx.cismain.jzd
             }}</span>
           </div>
           <div class="div-item-four">
-            电话：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            电话：<span style="text-decoration: underline">{{ jbxx.cismain.lxdh }}</span>
           </div>
           <div class="div-item-four">
-            邮编：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            邮编：<span style="text-decoration: underline">{{ jbxx.cismain.xzzyb }}</span>
           </div>
         </div>
         <div class="div-line-wrap">
           <div class="div-item-two">
             工作单位及地址：<span style="text-decoration: underline; width: 200px; display: inline-block">{{
-              jbxx.name
+              jbxx.cismain.gzdw
             }}</span>
           </div>
           <div class="div-item-four">
-            单位电话：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            单位电话：<span style="text-decoration: underline">{{ jbxx.cismain.gzdwdh }}</span>
           </div>
           <div class="div-item-four">
-            邮编：<span style="text-decoration: underline">{{ jbxx.name }}</span>
-          </div>
-        </div>
-
-        <div class="div-line-wrap">
-          <div class="div-item-four">
-            联系人姓名：<span style="text-decoration: underline">{{ jbxx.name }}</span>
-          </div>
-          <div class="div-item-four">
-            关系：<span style="text-decoration: underline">{{ jbxx.name }}</span>
-          </div>
-          <div class="div-item-four">
-            地址：<span style="text-decoration: underline">{{ jbxx.name }}</span>
-          </div>
-          <div class="div-item-four">
-            电话：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            邮编：<span style="text-decoration: underline">{{ jbxx.cismain.gzdwyb }}</span>
           </div>
         </div>
 
         <div class="div-line-wrap">
           <div class="div-item-four">
-            入院途径：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            联系人姓名：<span style="text-decoration: underline">{{ jbxx.cismain.lxrxm }}</span>
           </div>
           <div class="div-item-four">
-            入院时间：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            关系：<span style="text-decoration: underline">{{ jbxx.cismain.lxrgxmc }}</span>
           </div>
           <div class="div-item-four">
-            入院科别：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            地址：<span style="text-decoration: underline">{{ jbxx.cismain.lxrdz }}</span>
           </div>
           <div class="div-item-four">
-            病房：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            电话：<span style="text-decoration: underline">{{ jbxx.cismain.lxrdh }}</span>
           </div>
         </div>
 
         <div class="div-line-wrap">
           <div class="div-item-four">
-            转科：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            入院途径：<span style="text-decoration: underline">{{ jbxx.cismain.rylxmc }}</span>
+          </div>
+          <div class="div-item-four">
+            入院时间：<span style="text-decoration: underline">{{ jbxx.cismain.rysj }}</span>
+          </div>
+          <div class="div-item-four">
+            入院科别：<span style="text-decoration: underline">{{ jbxx.cismain.ryksmc }}</span>
+          </div>
+          <div class="div-item-four">
+            病房：<span style="text-decoration: underline">{{ jbxx.cismain.rybq }}</span>
+          </div>
+        </div>
+
+        <div class="div-line-wrap">
+          <div class="div-item-four">
+            转科：<span style="text-decoration: underline">{{ jbxx.zkmc || '-' }}</span>
           </div>
           <div class="div-item-four"></div>
           <div class="div-item-four"></div>
@@ -153,22 +152,22 @@
 
         <div class="div-line-wrap">
           <div class="div-item-four">
-            出院时间：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            出院时间：<span style="text-decoration: underline">{{ jbxx.cismain.cysj }}</span>
           </div>
           <div class="div-item-four">
-            出院科别：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            出院科别：<span style="text-decoration: underline">{{ jbxx.cismain.cyksmc }}</span>
           </div>
           <div class="div-item-four">
-            病房：<span style="text-decoration: underline">{{ jbxx.name }}</span>
+            病房：<span style="text-decoration: underline">{{ jbxx.cismain.cybq }}</span>
           </div>
           <div class="div-item-four">
-            实际住院：<span style="text-decoration: underline">{{ jbxx.name }}</span
+            实际住院：<span style="text-decoration: underline">{{ jbxx.cismain.zyts }}</span
             >&nbsp;天
           </div>
         </div>
       </div>
 
-      <div class="div-title" style="margin-top:10px">
+      <div class="div-title" style="margin-top: 10px">
         <div class="div-line-blue"></div>
         <span class="span-title">患者诊断</span>
       </div>
@@ -177,11 +176,11 @@
       <a-table
         ref="table"
         :pagination="false"
-        style="margin-top:10px"
+        style="margin-top: 10px"
         size="default"
         bordered
         :columns="columns"
-        :data-source="data"
+        :data-source="insideZdxx"
         :alert="true"
         :rowKey="(record) => record.code"
       >
@@ -201,49 +200,42 @@ import { getSoundRecordingList } from '@/api/modular/system/posManage'
 import { Timeline } from 'ant-design-vue'
 
 export default {
-  components: {
-    [Timeline.Item.name]: Timeline.Item,
-  },
+  components: {},
   props: {
     jbxx: Object,
+    patientInfo: Object,
   },
   data() {
     return {
-      showResultInfo: false,
-      audioSrc: '',
-      data: [
-        { id: 1, name: '检查' },
-        { id: 2, name: '检验' },
-      ],
+      insideZdxx: this.jbxx.zdxx,
       columns: [
         {
           title: '医院诊断类别名称',
-          width: 60,
-          // innerHeight:20,
-          dataIndex: 'name',
+          dataIndex: 'zdlbmc',
         },
         {
           title: '诊断编码',
-          width: 100,
-          dataIndex: 'd',
+          dataIndex: 'zdbm',
           ellipsis: true,
         },
         {
+          title: '医院诊断名称',
+          // innerHeight:20,
+          dataIndex: 'zdbmmc',
+        },
+        {
           title: '诊断时间',
-          width: 100,
-          dataIndex: 'userNframe',
+          dataIndex: 'zdsj',
           ellipsis: true,
         },
         {
           title: '主要诊断标志',
-          width: 100,
-          dataIndex: 'userfrgName',
+          dataIndex: 'zyzdbz',
           ellipsis: true,
         },
         {
           title: '疑似诊断标志',
-          width: 100,
-          dataIndex: 'usergrtName',
+          dataIndex: 'yzdbz',
           ellipsis: true,
         },
       ],
@@ -261,6 +253,8 @@ export default {
     //     this.$message.error(res.message)
     //   }
     // })
+    // this.patientInfo.baseInfo.birthday =
+    //   birthday.substring(0, 4) + '-' + birthday.substring(4, 6) + '-' + birthday.substring(6, 8)
   },
   methods: {
     formatDate(date) {
@@ -272,10 +266,14 @@ export default {
       myweekday < 10 ? (myweekday = '0' + myweekday) : myweekday
       return `${myyear}-${mymonth}-${myweekday}`
     },
+
+    refreshData(zdxx) {
+      this.insideZdxx = zdxx
+    },
   },
 }
 </script>
-<style lang="less">
+<style lang="less" scoped>
 .nodata {
   height: 90%;
   width: 99%;
