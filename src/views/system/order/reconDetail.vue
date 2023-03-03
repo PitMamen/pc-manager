260097cd6fb99703906afc8e8bff146ae96f8584
pxk <541931879@qq.com>
2023-03-03 14:11:30 +0800

    v

--- src/views/system/order/reconDetail.vue
+++ src/views/system/order/reconDetail.vue
@@ -680,15 +680,15 @@ export default {
 .ant-layout-content {
   height: calc(100vh - 82px) !important;
 }
-
 // 分页器置底，每个页面会有适当修改，修改内容为下面calc()中的px
 .ant-card {
-  height: calc(100% - 40px);
+  height: calc(100% - 20px);
   /deep/ .ant-card-body {
     height: 100%;
     padding-bottom: 10px !important;
+    margin-top: -20px;
     .table-wrapper {
-      height: calc(100% - 138px);
+      height: calc(100% - 238px);
       .ant-table-wrapper {
         height: 100%;
         .ant-spin-nested-loading {
