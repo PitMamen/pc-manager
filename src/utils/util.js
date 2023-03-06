56b7f9d1650a64e5e85e9447a0d55dbe9d0bd65e
fushengbo <shengbo.fu@bitvalue.com.cn>
2023-03-06 10:01:39 +0800

    拉取dev_1.7.3

--- src/utils/util.js
+++ src/utils/util.js
@@ -13,6 +13,7 @@ export function timeFix() {
 }
 
 export function welcome() {
+  
   const arr = ['休息一会儿吧', '准备吃什么呢?', '要不要打一把 LOL', '我猜你可能累了']
   const index = Math.floor(Math.random() * arr.length)
   return arr[index]
