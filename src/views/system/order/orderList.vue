6e4e174e348bcef2fe0accacd74feac3740520e4
pxk <541931879@qq.com>
2023-04-06 11:04:04 +0800

    订单管理时间修改

--- src/views/system/order/orderList.vue
+++ src/views/system/order/orderList.vue
@@ -292,6 +292,7 @@ export default {
       ],
       // 加载数据方法 必须为 Promise 对象
       loadData: (parameter) => {
+        console.log("JJH:",this.queryParams.orderStartTime,this.queryParams.orderEndTime)
         if (this.queryParams.orderStartTime && this.queryParams.orderEndTime) {
           if (this.queryParams.orderStartTime > this.queryParams.orderEndTime) {
             this.$message.error('请选择开始时间小于结束时间')
@@ -492,8 +493,8 @@ export default {
       this.queryParams.commodityName = ''
       this.queryParams.doctorName = ''
       this.queryParams.hospitalCode = undefined
-      this.queryParams.orderEndTime = ''
-      this.queryParams.orderStartTime = ''
+      // this.queryParams.orderEndTime = ''
+      // this.queryParams.orderStartTime = ''
       this.queryParams.classifyId = ''
       this.queryParams.orderStatus = ''
       this.handleOk()
