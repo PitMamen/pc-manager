a2b8a8c5ce1072ace0485e864e18c13cb0233c19
pxk <541931879@qq.com>
2023-07-19 11:03:41 +0800

    全院统计

--- src/views/system/treat/followDetail.vue
+++ src/views/system/treat/followDetail.vue
@@ -212,21 +212,21 @@ export default {
       //     specFlag: '', //随访标识  1 无需随访 0 应随访
       //     successFlag: '', //随访成功标识 1 实际随访人数  0  失访人数
       if (type == 1) {
-        this.qryFollowStatList.successFlag = 1
-        this.qryFollowStatList.openidFlag = ''
-        this.qryFollowStatList.specFlag = ''
+        this.queryParamsStatisit.successFlag = 1
+        this.queryParamsStatisit.openidFlag = ''
+        this.queryParamsStatisit.specFlag = ''
       } else if (type == 2) {
-        this.qryFollowStatList.specFlag = 0
-        this.qryFollowStatList.successFlag = ''
-        this.qryFollowStatList.openidFlag = ''
+        this.queryParamsStatisit.specFlag = 0
+        this.queryParamsStatisit.successFlag = ''
+        this.queryParamsStatisit.openidFlag = ''
       } else if (type == 3) {
-        this.qryFollowStatList.specFlag = 1
-        this.qryFollowStatList.successFlag = ''
-        this.qryFollowStatList.openidFlag = ''
+        this.queryParamsStatisit.specFlag = 1
+        this.queryParamsStatisit.successFlag = ''
+        this.queryParamsStatisit.openidFlag = ''
       } else if (type == 4) {
-        this.qryFollowStatList.specFlag = ''
-        this.qryFollowStatList.successFlag = 0
-        this.qryFollowStatList.openidFlag = ''
+        this.queryParamsStatisit.specFlag = ''
+        this.queryParamsStatisit.successFlag = 0
+        this.queryParamsStatisit.openidFlag = ''
       }
 
       this.$nextTick(() => {
