b79fac3cb326e131a19c1aa6381c8b8b6d578efc
pxk <541931879@qq.com>
2023-12-07 18:02:35 +0800

    b

--- src/views/system/tenantmanage/departmanage.vue
+++ src/views/system/tenantmanage/departmanage.vue
@@ -10,7 +10,7 @@
   >
     <a-spin :spinning="confirmLoading">
       <div class="div-part" style="margin-top: 0px">
-        <div class="div-part-left">
+        <div class="div-part-left" style="overflow:scroll;">
           <div
             class="left-content1"
             style="margin-bottom: 20px"
